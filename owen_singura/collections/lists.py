b8c9efbdde841e0f1cce19467902c8342063b1b3
jumashafara <jumashafara0@gmail.com>
2024-10-14 21:12:38 +0300

    added badru and irene

---
+++ owen_singura/collections/lists.py
@@ -0,0 +1,23 @@
+# Containers are objects that contain objects
+# Lists:
+    # A list sis an ordered collection of elements
+
+household_head_names = ['Juma Shafara', 'Owen Singura', 'Irene Katushabe']
+print(type(household_head_names))
+
+# indexing
+#   Is used to access the items of a list
+household_head_names = ['Juma Shafara', 'Owen Singura', 'Irene Katushabe']
+print(household_head_names[0])
+print(household_head_names[1])
+print(household_head_names[2])
+
+
+# Range of Indexes
+# Exercise
+
+# Adding items to a list
+
+
+
+
---
+++ owen_singura/collections/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+household_head_names = ['Juma Shafara', 'Owen Singura', 'Irene Katushabe']
+print(household_head_names[0])
+print(household_head_names[1])
+print(household_head_names[2])
